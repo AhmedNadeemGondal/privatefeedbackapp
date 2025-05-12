c4071a729d56bbc1b1a7ab82edaf62da947b61ee
ahmed.gondal <ahmednadeemgondal92@gmail.com>
2025-05-12 18:35:56 +0500

    video 5 complete

---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const messageSchema = z.object({
+  content: z
+    .string()
+    .min(10, "Content must be 10 characters")
+    .min(300, "Content must be between 10 and 300 characters"),
+});
